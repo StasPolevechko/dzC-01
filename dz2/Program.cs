5ce90349f619edc29bceeefae3af307911ced0e5
Stas <staspolevechko@gmail.com>
2023-04-14 10:39:29 +0300

    задача №2 готова

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+Console.Write("введите первое целое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write("введите второе целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNamber1 = int.Parse(userInput1);
+int userNamber2 = int.Parse(userInput2);
+if (userNamber1 > userNamber2)
+  {
+    Console.WriteLine($"a={userNamber1}, b={userInput2} -> max ={userNamber1}");
+  }
+else
+{
+    Console.WriteLine($"a={userNamber1}, b={userInput2} -> max ={userNamber2}");
+}
