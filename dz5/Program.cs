5a4fbfa46d692b0d43d485df5d09e2a8d0f283e1
Stas <staspolevechko@gmail.com>
2023-04-14 13:23:04 +0300

    задача№8 готова

---
+++ dz5/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.Write("введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNamber = int.Parse(userInput);
+for (int i = 1; i <= userNamber; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($" {userNamber} -> {i + ","} ");
+    }
+}
